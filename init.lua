603505b1054a8a4fbbcb12c97c581e76eb09680d
Denis <denis.junuzovic@fleetster.net>
2023-01-02 22:30:21 +0100

    customizing a bit and cleaning up

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
+
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     is_bootstrap = true
     vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
@@ -8,20 +9,14 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-    -- Package manager
     use 'wbthomason/packer.nvim'
 
     use { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
         requires = {
-            -- Automatically install LSPs to stdpath for neovim
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
-
-            -- Useful status updates for LSP
             'j-hui/fidget.nvim',
-
-            -- Additional lua configuration, makes nvim stuff amazing
             'folke/neodev.nvim',
         },
     }
@@ -31,42 +26,23 @@ require('packer').startup(function(use)
         requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
     }
 
-    use { -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            pcall(require('nvim-treesitter.install').update { with_sync = true })
-        end,
-    }
+    local function tree_sitter_install() pcall(require('nvim-treesitter.install').update { with_sync = true }) end
+    use { 'nvim-treesitter/nvim-treesitter', run = tree_sitter_install }
 
-    use { -- Additional text objects via treesitter
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-    }
-
-    -- Git related plugins
     use 'tpope/vim-fugitive'
     use 'tpope/vim-rhubarb'
     use 'lewis6991/gitsigns.nvim'
-
-    use 'ellisonleao/gruvbox.nvim'
-    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-    use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-    use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-    use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-
-    -- Fuzzy Finder (files, lsp, etc)
-    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
-    -- Go extras
-    use { 'fatih/vim-go', run = ':GoUpdateBinaries' }
-
-    -- Undoo tree
+    use 'nvim-lualine/lualine.nvim'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'numToStr/Comment.nvim'
+    use 'tpope/vim-sleuth'
     use 'mbbill/undotree'
 
-    -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
+    use { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' }
+    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+    use { 'fatih/vim-go', run = ':GoUpdateBinaries' }
+
     local has_plugins, plugins = pcall(require, 'custom.plugins')
     if has_plugins then
         plugins(use)
@@ -77,10 +53,6 @@ require('packer').startup(function(use)
     end
 end)
 
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
 if is_bootstrap then
     print '=================================='
     print '    Plugins are being installed'
@@ -90,7 +62,6 @@ if is_bootstrap then
     return
 end
 
--- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
     command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
@@ -98,45 +69,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = vim.fn.expand '$MYVIMRC',
 })
 
--- [[ Setting options ]]
--- See `:help vim.o`
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
-
--- My options
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.colorcolumn = '120'
-
--- Set colorscheme & background
-vim.o.termguicolors = true
-vim.cmd [[
-  colorscheme gruvbox
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -440,11 +373,28 @@ cmp.setup {
     },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
---
+vim.cmd [[
+  colorscheme kanagawa
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+]]
 
--- My keymaps
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.colorcolumn = '120'
+vim.o.termguicolors = true
+vim.o.splitbelow = true
+
 
 vim.keymap.set('v', '{', ':m \'>+1<cr>gv=gv')
 vim.keymap.set('v', '}', ':m \'<-2<cr>gv=gv')
